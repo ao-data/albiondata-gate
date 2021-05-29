1eab6a61997bc1b2ffc80bc94474714c9689b6da
Daniel Koch <koch@triple6.org>
2021-05-29 10:43:45 +0200

    cleanup

--- lib/nats.rb
+++ lib/nats.rb
@@ -2,19 +2,16 @@ require 'nats/io/client'
 
 class NATSForwarder
   LOGGER = Logger.new(STDOUT)
-
-  @@q = {}
-  TOPICS.each { |t| @@q[t] = [] }
   @@nats = NATS::IO::Client.new
 
-  def self.q; @@q; end
-  def self.nats; @@nats; end
+  
+  def self.start
+    @@nats.connect(NATS_URI, {verbose: true})
+  end
+
 
   def self.forward(topic, msg)
     @@nats.publish(topic, msg.to_json)
   end
 
-  def self.start
-    @@nats.connect(NATS_URI, {verbose: true})
-  end
 end
