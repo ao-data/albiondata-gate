c477755ef805120b23e58d6acbbb01b7cf9588c5
Daniel Koch <koch@triple6.org>
2021-05-29 10:42:29 +0200

    forward without q-ing

--- lib/nats.rb
+++ lib/nats.rb
@@ -2,27 +2,19 @@ require 'nats/io/client'
 
 class NATSForwarder
   LOGGER = Logger.new(STDOUT)
-  
+
   @@q = {}
   TOPICS.each { |t| @@q[t] = [] }
   @@nats = NATS::IO::Client.new
 
   def self.q; @@q; end
+  def self.nats; @@nats; end
+
+  def self.forward(topic, msg)
+    @@nats.publish(topic, msg.to_json)
+  end
 
   def self.start
-    Thread.new do
-      @@nats.connect(NATS_URI, {verbose: true})
-      while @@nats.connected?
-        sleep 5
-        TOPICS.each do |topic|
-          next if @@q[topic].count < 1
-          LOGGER.info("NATS") { "Forwarding Q of: #{topic} (#{@@q[topic].count} msgs)"  }
-          @@q[topic].each do |msg|
-            @@nats.publish(topic, msg.to_json)
-            @@q[topic].delete(msg)
-          end
-        end
-      end
-    end
+    @@nats.connect(NATS_URI, {verbose: true})
   end
 end
