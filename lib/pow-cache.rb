f58515d82b743947c18af4750ea93f541c74de3b
Daniel Koch <koch@triple6.org>
2021-06-05 10:09:12 +0200

    use pstore as pow storage instead of pure memory

--- lib/pow-cache.rb
+++ lib/pow-cache.rb
@@ -1,21 +1,40 @@
-$POWS = {}
-$POWS_SOLVED = []
+require 'pstore'
 
-#Limit the amount of pows to keep
-#Delete the oldest handed until threshold is reached
-Thread.new do
-  Thread.current.priority = -3 
-  loop do
-    begin
-      if ($POWS.count - POW_KEEP) > 0
-        $POWS.delete(*$POWS.keys.first($POWS.count - POW_KEEP))
-      end
+class PowStore
+  attr_reader :store
 
-      if ($POWS_SOLVED.count - POW_KEEP) > 0
-        $POWS_SOLVED.shift(($POWS_SOLVED.count - POW_KEEP))
-      end
-    ensure
-      sleep 120
+  def initialize(store=__dir__ + "/../pows.pstore")
+    @store = PStore.new(store)
+    @store.ultra_safe = true
+  end
+
+  def new(key:, wanted:)
+    @store.transaction do
+      @store[key] = {
+        wanted: wanted,
+        solved: false
+      }
+      @store.commit
     end
   end
+
+  def solved(key)
+    @store.transaction do
+      @store[key][:solved] = true
+      @store.commit
+    end
+  end
+
+  def delete(key)
+    @store.transaction do
+      @store.delete(key)
+      @store.commit
+    end
+  end
+
+  def [](key)
+    @store.transaction(true) do
+      @store[key]
+    end
+  end 
 end
