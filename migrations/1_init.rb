657f60122b73eb2f02ec8d819abdf73e89b0a93d
Daniel Koch <koch@triple6.org>
2021-06-05 12:38:59 +0200

    restore better version of the in-memory pow cache

--- migrations/1_init.rb
+++
@@ -1,9 +0,0 @@
-Sequel.migration do
-  up do
-    create_table :pows do
-      String :key, null: false, unique: true, primary_key: true
-      String :wanted
-      FalseClass :solved, null: false, default: false
-    end
-  end
-end
