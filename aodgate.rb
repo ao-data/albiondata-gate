978e90e5212a0f46b5c0a9b21455f39727854308
Paul Hendryx <phendryx@gmail.com>
2021-11-04 11:13:24 -0400

    redis pow and additional logging

--- aodgate.rb
+++ aodgate.rb
@@ -5,6 +5,7 @@ require 'sinatra'
 require 'securerandom'
 require 'digest/sha2'
 require 'rack/throttle'
+require 'redis'
 
 STDOUT.sync = true
 LOGGER = Logger.new(STDOUT)
@@ -31,6 +32,11 @@ class AODGate < Sinatra::Base
     set server: "puma"
   end
 
+  def initialize
+    super
+    @redis = Redis.new(url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}")
+  end
+
   use Rack::Throttle::Minute, :max => REQUEST_LIMIT[:per_minute]
   use Rack::Throttle::Hourly, :max => REQUEST_LIMIT[:per_hour]
   use Rack::Throttle::Daily, :max => REQUEST_LIMIT[:per_day]
@@ -40,15 +46,18 @@ class AODGate < Sinatra::Base
 
   get '/pow' do
     challange = { wanted: SecureRandom.hex(POW_RANDOMNESS).unpack("B*")[0][0..POW_DIFFICULITY-1], key: SecureRandom.hex(POW_RANDOMNESS) }
-    $POW_MUTEX.synchronize { $POWS[challange[:key]] = {wanted: challange[:wanted]} }
+    @redis.set(challange[:key], {wanted: challange[:wanted]}.to_json, ex: ENV['POW_EXPIRE_SECONDS'].to_i)
     return challange.to_json
   end
 
   post '/pow/:topic' do
     halt 404 unless TOPICS.include?(params[:topic])
-    pow = $POWS[params[:key]]
-    halt(902, "Pow not handed") unless pow # This pow was never requested
-    halt(903, "Pow not solved correctly") unless Digest::SHA2.hexdigest("aod^" + params[:solution] + "^" + params[:key]).unpack("B*")[0].start_with?(pow[:wanted])
+    pow_json = @redis.get(params[:key])
+    @redis.del(params[:key])
+    halt(902, "Pow not handed") unless pow_json # This pow was never requested or has expired
+    pow = JSON.parse(pow_json)
+
+    halt(903, "Pow not solved correctly") unless Digest::SHA2.hexdigest("aod^" + params[:solution] + "^" + params[:key]).unpack("B*")[0].start_with?(pow['wanted'])
     halt(904, "Payload too large") unless params[:natsmsg].bytesize <= NATS_PAYLOAD_MAX
 
     begin
@@ -57,19 +66,33 @@ class AODGate < Sinatra::Base
       halt(901, "Invalid JSON data")
     end
 
-    halt(904, "Too much data") if params[:topic] == "marketorders.ingest" and data['Orders'].count > 50
-    halt(904, "Too much data") if params[:topic] == "goldprices.ingest" && data['Prices'].count > 673
+    if params[:topic] == "marketorders.ingest" && data['Orders'].count > 50
+      LOGGER.warn("Error 904, Too Much Data. ip: #{request.ip}, topic: marketorders.ingest, order count: #{data['Orders'].count}")
+      halt(904, "Too much data")
+    end
+
+    if params[:topic] == "goldprices.ingest" && data['Prices'].count > 673
+      LOGGER.warn("Error 904, Too Much Data. ip: #{request.ip}, topic: goldprices.ingest, order count: #{data['Prices'].count}")
+      halt(904, "Too much data")
+    end
 
     if params[:topic] == "markethistories.ingest"
-      halt(904, "Too much data") if data['Timescale'] == 0 && data['MarketHistories'].count > 24
-      halt(904, "Too much data") if data['Timescale'] == 1 && data['MarketHistories'].count > 29
-      halt(904, "Too much data") if data['Timescale'] == 2 && data['MarketHistories'].count > 113
+      failed = false
+
+      failed = true if data['Timescale'] == 0 && data['MarketHistories'].count > 24
+      failed = true if data['Timescale'] == 1 && data['MarketHistories'].count > 29
+      failed = true if data['Timescale'] == 2 && data['MarketHistories'].count > 113
+
+
+      if failed == true
+        LOGGER.warn("Error 904, Too Much Data. ip: #{request.ip}, topic: markethistories.ingest, Timescale: #{data['Timescale']}, MarketHistories count: #{data['MarketHistories'].count}")
+        halt(904, "Too much data")
+      end
     end
 
     NATSForwarder.forward(params[:topic], data)
     $POW_MUTEX.synchronize { $POWS.delete(params[:key]) }
     halt(200, "OK")
-
   end
 end
 
